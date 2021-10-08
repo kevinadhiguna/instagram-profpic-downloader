286d6ac8369bdf668fe53f39518722705d813645
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-08 06:20:43 +0000

    feat: add a banner
    also added a repo link

--- insta.py
+++ insta.py
@@ -1,6 +1,12 @@
 import instaloader
+import pyfiglet
 
 instagram = instaloader.Instaloader()
+
+# Print a banner
+ascii_banner = pyfiglet.figlet_format("Insta Profpic")
+print(ascii_banner)
+print("GitHub Repo : https://github.com/kevinadhiguna/instagram-profpic-downloader\n")
 
 try:
     instagram_account = input("What is the account name ? : ")
